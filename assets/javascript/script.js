1ccf38b1492c1a06e0e481e436d9c5322f1ab26c
Your Name <james.edward.pritchard@gmail.com>
2022-06-02 12:19:38 -0400

    changed fetch requests, gets all data and saves to array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,210 +1,344 @@
-var CHAR_Luke;
-var CHAR_Leia;
-var CHAR_C3po;
-var CHAR_HanSolo;
-var CHAR_Chewy;
-var CHAR_Yoda;
-var CHAR_Anikin;
-var CHAR_ObiWan;
+var peopleArray = [];
+var speciesArray = [];
+var planetsArray = [];
+var starshipsArray = [];
+var filmsArray = [];
 
-var SPECIES_Human;
-var SPECIES_Droid;
-var SPECIES_Wookie;
-var SPECIES_Yoda;
-var SPECIES_Ewok;
+//these need to be verified as the idex was relating to the page number 
+//could be as easy as adding 10 to the index for each page
+var CHAR_Luke                   = peopleArray[0];
+var CHAR_Leia                   = peopleArray[4];
+var CHAR_C3po                   = peopleArray[1];
+var CHAR_ObiWan                 = peopleArray[9];
+var CHAR_Anikin                 = peopleArray[0];
+var CHAR_Chewy                  = peopleArray[2];
+var CHAR_HanSolo                = peopleArray[3];
+var CHAR_Yoda                   = peopleArray[8];
 
-var PLANETS_Tatooine;
-var PLANETS_Alderaan;
-var PLANETS_Dagobah;
-var PLANETS_Coruscant;
-var PLANETS_Hoth;
-var PLANETS_Endor;
-var PLANETS_Naboo;
+var SPECIES_Human               = speciesArray[0];
+var SPECIES_Droid               = speciesArray[1];
+var SPECIES_Wookie              = speciesArray[2];
+var SPECIES_Yoda                = speciesArray[5];
+var SPECIES_Ewok                = speciesArray[8];
+
+var PLANETS_Tatooine            = planetsArray[0];
+var PLANETS_Alderaan            = planetsArray[1];
+var PLANETS_Hoth                = planetsArray[3];
+var PLANETS_Dagobah             = planetsArray[4];
+var PLANETS_Endor               = planetsArray[6];
+var PLANETS_Naboo               = planetsArray[7];
+var PLANETS_Coruscant           = planetsArray[8];                
+
+var STARSHIPS_Cr90Corvette      = starshipsArray[0];
+var STARSHIPS_StarDestroyer     = starshipsArray[1];
+var STARSHIPS_DeathStar         = starshipsArray[3];
+var STARSHIPS_MillenniumFalcon  = starshipsArray[4];
+var STARSHIPS_YWing             = starshipsArray[5];
+var STARSHIPS_XWing             = starshipsArray[6];
+var STARSHIPS_TieAdvancedX1     = starshipsArray[7];
+var STARSHIPS_DroidControlShip  = starshipsArray[7];
+var STARSHIPS_NabooFighter      = starshipsArray[8];
+var STARSHIPS_NabooRoyalStarship= starshipsArray[9];
+var STARSHIPS_DroidControlShip  = starshipsArray[7];
+var STARSHIPS_NabooFighter      = starshipsArray[8];
+var STARSHIPS_NabooRoyalStarship= starshipsArray[9];
+
+var FILMS_ANewHope              = filmsArray[0];
+var FILMS_TheEmpireStrikesBack  = filmsArray[1];
+var FILMS_ReturnOfTheJedi       = filmsArray[2];
+var FILMS_ThePhantomMenace      = filmsArray[3];
+var FILMS_AttackOfTheClones     = filmsArray[4];
+var FILMS_RevengeOfTheSith      = filmsArray[5];
 
 
-var STARSHIPS_Cr90Corvette;
-var STARSHIPS_StarDestroyer;
-var STARSHIPS_DeathStar;
-var STARSHIPS_MillenniumFalcon;
-var STARSHIPS_YWing
-var STARSHIPS_XWing
-var STARSHIPS_TieAdvancedX1;
-var STARSHIPS_DroidControlShip;
-var STARSHIPS_NabooFighter;
-var STARSHIPS_NabooRoyalStarship;
-
-var FILMS_ANewHope;
-var FILMS_TheEmpireStrikesBack;
-var FILMS_ReturnOfTheJedi;
-var FILMS_ThePhantomMenace;
-var FILMS_AttackOfTheClones;
-var FILMS_RevengeOfTheSith;
-
-
-function fetchMainCharacters(){
+function fetchAllData(){
+    //Fetch All People
     fetch(`https://swapi.dev/api/people`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        //Pull main characters from page one
-        CHAR_Luke = data.results[0];
-        CHAR_Leia = data.results[4];
-        CHAR_C3po = data.results[1];
-        CHAR_ObiWan = data.results[9];
+        for (let i = 0; i < data.results.length; i++) {
+            let people = data.results[i];
+            peopleArray.push(people)
+        }
         
     }).then(function(){
-        //Pull main characters from page two
         fetch(`https://swapi.dev/api/people?page=2`)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            CHAR_Anikin = data.results[0];
-            CHAR_Chewy = data.results[2];
-            CHAR_HanSolo = data.results[3];
-            CHAR_Yoda = data.results[8];
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)
+            }
+        })
     }).then(function(){
-        localStorage.setItem('MainCharacters', 
-            JSON.stringify([
-                CHAR_Luke,
-                CHAR_Leia,
-                CHAR_C3po,
-                CHAR_ObiWan,
-                CHAR_Anikin,
-                CHAR_Chewy,
-                CHAR_HanSolo
-                ,CHAR_Yoda
-            ]))
-    })
-})
-}
-function fetchMainSpecies(){
-    fetch(`https://swapi.dev/api/species`)
+        fetch(`https://swapi.dev/api/people?page=3`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/people?page=4`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/people?page=5`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/people?page=6`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/people?page=7`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/people?page=8`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/people?page=9`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let people = data.results[i];
+                peopleArray.push(people)    
+            }
+        })
+
+
+
+
+
+        //fetch all species
+    }).then(function(){
+        fetch(`https://swapi.dev/api/species`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        //Pull main species from page one
-        SPECIES_Human = data.results[0];
-        SPECIES_Droid = data.results[1];
-        SPECIES_Wookie = data.results[2];
-        SPECIES_Yoda = data.results[5];
-        SPECIES_Ewok = data.results[8];
+        for (let i = 0; i < data.results.length; i++) {
+            let species = data.results[i];
+            speciesArray.push(species)
+        }
+        
     }).then(function(){
-        localStorage.setItem('MainSpecies', 
-            JSON.stringify([
-                SPECIES_Human,
-                SPECIES_Droid,
-                SPECIES_Wookie,
-                SPECIES_Yoda,
-                SPECIES_Ewok,
-
-            ]))
+        fetch(`https://swapi.dev/api/species?page=2`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let species = data.results[i];
+                speciesArray.push(species)
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/species?page=3`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let species = data.results[i];
+                speciesArray.push(species)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/species?page=4`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let species = data.results[i];
+                speciesArray.push(species)    
+            }
+        })
     })
-}
-function fetchMainPlanets(){
-    fetch(`https://swapi.dev/api/planets`)
+
+
+
+        //fetch all planets
+    }).then(function(){
+        fetch(`https://swapi.dev/api/planets`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        //Pull main planets from page one
-        PLANETS_Tatooine = data.results[0];
-        PLANETS_Alderaan = data.results[1];
-        PLANETS_Hoth = data.results[3];
-        PLANETS_Dagobah = data.results[4];
-        PLANETS_Endor = data.results[6];
-        PLANETS_Naboo = data.results[7];
-        PLANETS_Coruscant = data.results[8];
+        for (let i = 0; i < data.results.length; i++) {
+            let planets = data.results[i];
+            planetsArray.push(planets)
+        }
+        
     }).then(function(){
-        localStorage.setItem('MainPlanets', 
-            JSON.stringify([
-                PLANETS_Tatooine,
-                PLANETS_Alderaan,
-                PLANETS_Hoth,
-                PLANETS_Dagobah,
-                PLANETS_Endor,
-                PLANETS_Naboo,
-                PLANETS_Coruscant
+        fetch(`https://swapi.dev/api/planets?page=2`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let planets = data.results[i];
+                planetsArray.push(planets)
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/planets?page=3`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let planets = data.results[i];
+                planetsArray.push(planets)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/planets?page=4`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let planets = data.results[i];
+                planetsArray.push(planets)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/planets?page=5`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let planets = data.results[i];
+                planetsArray.push(planets)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/planets?page=6`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let planets = data.results[i];
+                planetsArray.push(planets)    
+            }
+        })
 
-            ]))
-    })
-}
-function fetchMainStarships(){
-    fetch(`https://swapi.dev/api/starships`)
+        //fetch all starships
+    }).then(function(){
+        fetch(`https://swapi.dev/api/starships`)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        //Pull main starships from page one
-        STARSHIPS_Cr90Corvette = data.results[0];
-        STARSHIPS_StarDestroyer = data.results[1];
-        STARSHIPS_DeathStar = data.results[3];
-        STARSHIPS_MillenniumFalcon = data.results[4];
-        STARSHIPS_YWing= data.results[5];
-        STARSHIPS_XWing= data.results[6];
-        STARSHIPS_TieAdvancedX1= data.results[7];
-
-fetchMainCharacters();
-fetchMainSpecies();
-fetchMainPlanets();
-
-
+        for (let i = 0; i < data.results.length; i++) {
+            let starships = data.results[i];
+            starshipsArray.push(starships)
+        }
+        
     }).then(function(){
-        //Pull main starships from page two
         fetch(`https://swapi.dev/api/starships?page=2`)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            STARSHIPS_DroidControlShip = data.results[7];
-            STARSHIPS_NabooFighter = data.results[8];
-            STARSHIPS_NabooRoyalStarship = data.results[9];
-    }).then(function(){
-        localStorage.setItem('MainStarships', 
-            JSON.stringify([
-                STARSHIPS_Cr90Corvette,
-                STARSHIPS_StarDestroyer,
-                STARSHIPS_DeathStar,
-                STARSHIPS_MillenniumFalcon,
-                STARSHIPS_YWing,
-                STARSHIPS_XWing,
-                STARSHIPS_TieAdvancedX1,
-                STARSHIPS_DroidControlShip,
-                STARSHIPS_NabooFighter,
-                STARSHIPS_NabooRoyalStarship
-            ]))
+            for (let i = 0; i < data.results.length; i++) {
+                let starships = data.results[i];
+                starshipsArray.push(starships)
+            }
         })
-    })
-}
-function fetchFilms(){
-    fetch(`https://swapi.dev/api/films`)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        //Pull films from page one
-        FILMS_ANewHope = data.results[0];
-        FILMS_TheEmpireStrikesBack = data.results[1];
-        FILMS_ReturnOfTheJedi = data.results[2];
-        FILMS_ThePhantomMenace = data.results[3];
-        FILMS_AttackOfTheClones = data.results[4];
-        FILMS_RevengeOfTheSith = data.results[5];
     }).then(function(){
-        localStorage.setItem('Films', 
-            JSON.stringify([
-                FILMS_ANewHope,
-                FILMS_TheEmpireStrikesBack,
-                FILMS_ReturnOfTheJedi,
-                FILMS_ThePhantomMenace,
-                FILMS_AttackOfTheClones,
-                FILMS_RevengeOfTheSith,
-            ]))
-    })
+        fetch(`https://swapi.dev/api/starships?page=3`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let starships = data.results[i];
+                starshipsArray.push(starships)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/starships?page=4`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let starships = data.results[i];
+                starshipsArray.push(starships)    
+            }
+        })
+    }).then(function(){
+        fetch(`https://swapi.dev/api/films`)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.results.length; i++) {
+                let films = data.results[i];
+                filmsArray.push(films)    
+            }
+        })    
+    }).then(function(){
+
+        console.log(peopleArray),
+        console.log(speciesArray),
+        console.log(planetsArray),
+        console.log(starshipsArray),
+        console.log(filmsArray)
+        })  
+})})
 }
-fetchMainCharacters();
-fetchMainSpecies();
-fetchMainPlanets();
-fetchMainStarships();
-fetchFilms();
+
+fetchAllData();
+
