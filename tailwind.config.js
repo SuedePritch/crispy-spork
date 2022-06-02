671be23e0bf67962e20d4310ad32e49e61aae16e
Ryan Acham <ryanacham@hotmail.com>
2022-06-02 10:47:07 -0400

    Initial CSS Shtuff

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        starJedi: ['star_jediregular'],
+        specSC: ['Spectral SC', serif],
+      }
+    },
+  },
+  plugins: [],
+}
