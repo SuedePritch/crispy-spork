0f40ca3ce534bb130126ca3ae49cf3a1e2c386a6
Your Name <james.edward.pritchard@gmail.com>
2022-06-03 12:13:41 -0400

    all topic lists populate

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -62,12 +62,8 @@ var FILMS_ThePhantomMenace      = filmsArray[3];
 var FILMS_AttackOfTheClones     = filmsArray[4];
 var FILMS_RevengeOfTheSith      = filmsArray[5];
 
-// function sortArray(a,b){
-//     console.log(a.height, b.height);
-//     return a.height - b.height
-// }
 
-function fetchAllData(){
+function fetchAllPeople(){
     //Fetch All People
     for (let i = 1; i < 83; i++) {
         fetch(`https://swapi.dev/api/people/${i}`)
@@ -82,58 +78,108 @@ function fetchAllData(){
         })
     }
 }
-    
-    
+peopleTopicEl.addEventListener('click', function(){
+    fullListEl.innerHTML = '';
+    modalEl.style.display = 'block';
+    modalTitleEL.textContent = peopleTopicEl.textContent
+    for (let i = 0; i < 83; i++) {
+        const peopleListName = JSON.parse(localStorage.getItem(`https://swapi.dev/api/people/${i}/`))
+        var peopleListItem = $("<li>")
+        if(peopleListName){
+            peopleListItem.text(peopleListName.name)
+            peopleListItem.addClass('full-list-item')
+            $("#full-list").append(peopleListItem)
+        }
+    }
 
-
-
-
-
-        //fetch all species
-    // }).then(function(){
-    //     fetch(`https://swapi.dev/api/species`)
-    // .then(function (response) {
-    //     return response.json();
-    // })
-    // .then(function (data) {
-    //     for (let i = 0; i < data.results.length; i++) {
-    //         let species = data.results[i];
-    //         speciesArray.push(species)
-    //     }
-    //     //fetch all planets
-    // }).then(function(){
-    //     fetch(`https://swapi.dev/api/planets`)
-    // .then(function (response) {
-    //     return response.json();
-    // })
-    // .then(function (data) {
-    //     for (let i = 0; i < data.results.length; i++) {
-    //         let planets = data.results[i];
-    //         planetsArray.push(planets)
-    //     }
-    //     //fetch all starships
-    // }).then(function(){
-    //     fetch(`https://swapi.dev/api/starships`)
-    // .then(function (response) {
-    //     return response.json();
-    // })
-    // .then(function (data) {
-    //     for (let i = 0; i < data.results.length; i++) {
-    //         let starships = data.results[i];
-    //         starshipsArray.push(starships)
-    //     }
-    // }).then(function(){
-    //     fetch(`https://swapi.dev/api/films`)
-    //     .then(function (response) {
-    //         return response.json();
-    //     })
-    //     .then(function (data) {
-    //         for (let i = 0; i < data.results.length; i++) {
-    //             let films = data.results[i];
-    //             filmsArray.push(films)    
-    //         }
-    //     })    
-    // })
+})
+function fetchAllSpecies(){
+    //Fetch All Species
+    for (let i = 1; i < 38; i++) {
+        fetch(`https://swapi.dev/api/species/${i}`)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            let species = data;
+            
+            if(species){
+                localStorage.setItem(species.url, JSON.stringify(species))
+            }
+        })
+    }
+}
+speciesTopicEl.addEventListener('click', function(){
+    fullListEl.innerHTML = '';
+    modalEl.style.display = 'block';
+    modalTitleEL.textContent = speciesTopicEl.textContent
+    for (let i = 0; i < 38; i++) {
+        const speciesListName = JSON.parse(localStorage.getItem(`https://swapi.dev/api/species/${i}/`))
+        var speciesListItem = $("<li>")
+        if(speciesListName){
+            speciesListItem.text(speciesListName.name)
+            speciesListItem.addClass('full-list-item')
+            $("#full-list").append(speciesListItem)
+        }
+    }
+})
+function fetchAllPlanets(){
+    //Fetch All Planets
+    for (let i = 1; i < 30; i++) {
+        fetch(`https://swapi.dev/api/planets/${i}`)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            let planets = data;
+            
+            if(planets){
+                localStorage.setItem(planets.url, JSON.stringify(planets))
+            }
+        })
+    }
+}
+planetsTopicEl.addEventListener('click', function(){
+    fullListEl.innerHTML = '';
+    modalEl.style.display = 'block';
+    modalTitleEL.textContent = planetsTopicEl.textContent
+    for (let i = 0; i < 30; i++) {
+        const planetsListName = JSON.parse(localStorage.getItem(`https://swapi.dev/api/planets/${i}/`))
+        var planetsListItem = $("<li>")
+        if(planetsListName){
+            planetsListItem.text(planetsListName.name)
+            planetsListItem.addClass('full-list-item')
+            $("#full-list").append(planetsListItem)
+        }
+    }
+})
+function fetchAllStarships(){
+    //Fetch All Starships
+    for (let i = 1; i < 80; i++) {
+        fetch(`https://swapi.dev/api/starships/${i}`)
+        .then(function (response) {
+            return response.json();
+        }).then(function (data) {
+            let starships = data;
+            
+            if(starships){
+                localStorage.setItem(starships.url, JSON.stringify(starships))
+            }
+        })
+    }
+}
+starshipsTopicEl.addEventListener('click', function(){
+    fullListEl.innerHTML = '';
+    modalEl.style.display = 'block';
+    modalTitleEL.textContent = starshipsTopicEl.textContent
+    for (let i = 0; i < 80; i++) {
+        const starshipsListName = JSON.parse(localStorage.getItem(`https://swapi.dev/api/starships/${i}/`))
+        var starshipsListItem = $("<li>")
+        if(starshipsListName){
+            starshipsListItem.text(starshipsListName.name)
+            starshipsListItem.addClass('full-list-item')
+            $("#full-list").append(starshipsListItem)
+        }
+    }
+})
 
 function wikiAPI(){
     //this url will return a list of best matches and pageid
@@ -166,35 +212,10 @@ function wikiAPI(){
 //Event listeners for topic modal
 //opens and closes modal
 
-peopleTopicEl.addEventListener('click', function(){
-    fullListEl.innerHTML = '';
-    modalEl.style.display = 'block';
-    // peopleArray.sort(sortArray)
-    console.log(peopleArray);
-    modalTitleEL.textContent = peopleTopicEl.textContent
-    for (let i = 0; i < 83; i++) {
-        const peopleListName = JSON.parse(localStorage.getItem(`https://swapi.dev/api/people/${i}/`))
-        var peopleListItem = $("<li>")
-        if(peopleListName){
-            peopleListItem.text(peopleListName.name)
-            peopleListItem.addClass('full-list-item')
-            $("#full-list").append(peopleListItem)
-        }
-    }
 
-})
-speciesTopicEl.addEventListener('click', function(){
-    modalEl.style.display = 'block';
-    console.log('click');
-})
-planetsTopicEl.addEventListener('click', function(){
-    modalEl.style.display = 'block';
-    console.log('click');
-})
-starshipsTopicEl.addEventListener('click', function(){
-    modalEl.style.display = 'block';
-    console.log('click');
-})
+
+
+
 
 
 
@@ -207,7 +228,13 @@ window.onclick = function(event) {
     }
 }
 
+function onInit(){
 
-fetchAllData();
+    fetchAllPeople();
+    fetchAllSpecies();
+    fetchAllPlanets();
+    fetchAllStarships();
+}
+onInit();
 // wikiAPI();
 
